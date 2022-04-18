93e273fdeba1e6352a022b7243a2d4d5df1493a7
gruberx <gruberxr@gmail.com>
2022-04-18 16:16:33 +0300

    sc for near spring

--- src/lib.rs
+++ src/lib.rs
@@ -3,36 +3,26 @@ use near_sdk::near_bindgen;
 
 #[near_bindgen]
 #[derive(Default, BorshDeserialize, BorshSerialize)]
-pub struct Contract {
+pub struct Gatekeeper {
+    greeting: String
     // SETUP CONTRACT STATE
 }
 
 #[near_bindgen]
-impl Contract {
-    // ADD CONTRACT METHODS HERE
-}
-
-/*
- * the rest of this file sets up unit tests
- * to run these, the command will be:
- * cargo test --package rust-template -- --nocapture
- * Note: 'rust-template' comes from Cargo.toml's 'name' key
- */
-
-// use the attribute below for unit tests
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use near_sdk::test_utils::{get_logs, VMContextBuilder};
-    use near_sdk::{testing_env, AccountId};
-
-    // part of writing unit tests is setting up a mock context
-    // provide a `predecessor` here, it'll modify the default context
-    fn get_context(predecessor: AccountId) -> VMContextBuilder {
-        let mut builder = VMContextBuilder::new();
-        builder.predecessor_account_id(predecessor);
-        builder
+impl Gatekeeper {
+    #[init]
+    pub fn set_default_greeting() -> Self {
+        Self{
+            greeting: "Hello ".to_string(),
+        }
     }
 
-    // TESTS HERE
+    pub fn set_greeting(&mut self, wish: String) {
+        self.greeting = wish + " ";
+    }
+
+    pub fn receive_and_give(&self, name: String) -> String {
+        return self.greeting.clone() + &name + "!";
+    }
+    // ADD CONTRACT METHODS HERE
 }
